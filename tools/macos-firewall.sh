127da2d187d5aebe2accea3231e8f72adcc96daf
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-12-03 11:05:56 +0100

    tools: remove .libs/run-tests

--- tools/macos-firewall.sh
+++ tools/macos-firewall.sh
@@ -13,12 +13,7 @@ OUTDIR="$TOOLSDIR/../out"
 # rules are simply appended. By using pwd we can get the full path
 # without '..' and things work as expected.
 OUTDIR="`( cd \"$OUTDIR\" && pwd) `"
-PROJECT_DIR="$TOOLSDIR/../"
-PROJECT_DIR="`( cd \"$PROJECT_DIR\" && pwd) `"
 TESTS_DEBUG="$OUTDIR/Debug/run-tests"
-TESTS_LIBS="$PROJECT_DIR/test/.libs/run-tests"
-echo $TESTS_DEBUG
-echo $TESTS_LIBS
 
 if [ -f $SFW ];
 then
@@ -26,15 +21,10 @@ then
   # linked to either out/Debug/node or out/Release/node depending on the
   # BUILDTYPE.
   $SFW --remove "$TESTS_DEBUG"
-  # This will cause a segment fault, perhaps because of the dot in the 
-  # name of the file. 
-  #$SFW --remove "$TEST_LIBS"
 
   $SFW --add "$TESTS_DEBUG"
-  $SFW --add "$TESTS_LIBS"
 
   $SFW --unblock "$TESTS_DEBUG"
-  $SFW --unblock "$TESTS_LIBS"
 else
   echo "SocketFirewall not found in location: $SFW"
 fi
